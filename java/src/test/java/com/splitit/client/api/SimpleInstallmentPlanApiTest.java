e631448fecfc5f8282c440ab6584f4890cb8d81a
dphuang2 <dylan.p.huang@gmail.com>
2022-12-15 13:17:42 -0800

    update test based on newest spec

--- java/src/test/java/com/splitit/client/api/SimpleInstallmentPlanApiTest.java
+++ java/src/test/java/com/splitit/client/api/SimpleInstallmentPlanApiTest.java
@@ -4,6 +4,7 @@ import com.splitit.client.ApiClient;
 import com.splitit.client.ApiException;
 import com.splitit.client.Configuration;
 import com.splitit.client.auth.ApiKeyAuth;
+import com.splitit.client.auth.OAuth;
 import com.splitit.client.model.InstallmentPlanCreateRequest;
 import com.splitit.client.model.InstallmentPlanModel;
 import org.junit.Test;
@@ -19,17 +20,15 @@ public class SimpleInstallmentPlanApiTest {
     public void createInstallmentPlanTest() throws ApiException {
         ApiClient defaultClient = Configuration.getDefaultApiClient();
         defaultClient.setBasePath("http://127.0.0.1:4010");
-        ApiKeyAuth idempotencyKey = (ApiKeyAuth) defaultClient.getAuthentication("idempotencyKey");
-        ApiKeyAuth oauthKey = (ApiKeyAuth) defaultClient.getAuthentication("oauthKey");
-        idempotencyKey.setApiKey("YOUR API KEY");
-        oauthKey.setApiKey("YOUR API KEY");
+        OAuth oauthKey = (OAuth) defaultClient.getAuthentication("bearer");
+        oauthKey.setAccessToken("YOUR API KEY");
         InstallmentPlanApi api = new InstallmentPlanApi(defaultClient);
         InstallmentPlanCreateRequest installmentPlanCreateRequest = new InstallmentPlanCreateRequest();
         installmentPlanCreateRequest.setAutoCapture(true);
         installmentPlanCreateRequest.setAttempt3dSecure(true);
         installmentPlanCreateRequest.setAttemptAuthorize(true);
         installmentPlanCreateRequest.setTermsAndConditionsAccepted(true);
-        InstallmentPlanModel model = api.post(installmentPlanCreateRequest, null);
+        InstallmentPlanModel model = api.post("YOUR IDEMPOTENCY KEY", installmentPlanCreateRequest, null);
         assertNotNull("Received null response", model);
     }
 }
