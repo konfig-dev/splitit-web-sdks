e67705eb123980e1cff01d6f1b1ad048668e6713
dphuang2 <dylan.p.huang@gmail.com>
2022-12-15 17:05:45 -0800

    regenerate 1.0.2 w/ repository

--- csharp/src/Splitit.Net/Client/Configuration.cs
+++ csharp/src/Splitit.Net/Client/Configuration.cs
@@ -29,7 +29,7 @@ namespace Splitit.Net.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -114,7 +114,7 @@ namespace Splitit.Net.Client
         /// </summary>
         public Configuration()
         {
-            UserAgent = "Konfig/1.0.1/csharp";
+            UserAgent = "Konfig/1.0.2/csharp";
             BasePath = "https://web-api-v3.sandbox.splitit.com";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -187,7 +187,7 @@ namespace Splitit.Net.Client
             string tempFolderPath = null,
             string dateTimeFormat = null,
             int timeout = 100000,
-            string userAgent = "Konfig/1.0.1/csharp"
+            string userAgent = "Konfig/1.0.2/csharp"
             // ReSharper restore UnusedParameter.Local
             )
         {
@@ -421,7 +421,7 @@ namespace Splitit.Net.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0.0\n";
-            report += "    SDK Package Version: 1.0.1\n";
+            report += "    SDK Package Version: 1.0.2\n";
 
             return report;
         }
