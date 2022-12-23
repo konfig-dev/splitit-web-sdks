a99f1c297dd5ad96cd804a8c04a22d4720d1df0f
dphuang2 <dylan.p.huang@gmail.com>
2022-12-22 17:02:06 -0800

    add test case

---
+++ typescript/src/index.test.ts
@@ -0,0 +1,26 @@
+import { SplititClient } from "./SplititClient";
+import fetch from "node-fetch";
+it("simple operation started", async () => {
+  const clientId = process.env.SPLITIT_CLIENT_ID;
+  const clientSecret = process.env.SPLITIT_CLIENT_SECRET;
+
+  const oauthResponse = await fetch(
+    "https://id.sandbox.splitit.com/connect/token",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    }
+  );
+  const json = await oauthResponse.json();
+  const accessToken = json.access_token;
+  const client = new SplititClient({ TOKEN: accessToken });
+  const response = await client.installmentPlan.search({
+    xSplititIdempotencyKey: "1231231",
+    installmentPlanNumber: "21321",
+    refOrderNumber: "21312321",
+  });
+  expect(response).not.toBeNull();
+});
