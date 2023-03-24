cb920427ee9d752fe7603b728b76c90f22f9fed9
dphuang2 <dylan.p.huang@gmail.com>
2023-03-24 00:19:20 -0700

    update test

--- java/src/test/java/com/konfigthis/splitit/client/api/SimpleInstallmentPlanApiTest.java
+++ java/src/test/java/com/konfigthis/splitit/client/api/SimpleInstallmentPlanApiTest.java
@@ -2,7 +2,6 @@ package com.konfigthis.splitit.client.api;
 
 import com.konfigthis.splitit.client.ApiClient;
 import com.konfigthis.splitit.client.ApiException;
-import com.konfigthis.splitit.client.auth.OAuth;
 import com.konfigthis.splitit.client.model.*;
 import org.junit.jupiter.api.Test;
 
@@ -13,48 +12,35 @@ import static org.junit.Assert.assertNotNull;
 
 public class SimpleInstallmentPlanApiTest {
 
-    /**
-     * @throws ApiException if the Api call fails
-     */
-    @Test
-    public void createInstallmentPlanTest() throws ApiException {
-        String clientId = System.getenv("SPLITIT_CLIENT_ID");
-        String secretId = System.getenv("SPLITIT_CLIENT_SECRET");
-        ApiClient defaultClient = new ApiClient(clientId, secretId, null);
-        InstallmentPlanApi api = new InstallmentPlanApi(defaultClient);
-        InstallmentPlanCreateRequest installmentPlanCreateRequest = new InstallmentPlanCreateRequest(); // InstallmentPlanCreateRequest |
-        installmentPlanCreateRequest.attempt3dSecure(true)
-                .autoCapture(true)
-                .attemptAuthorize(true)
-                .termsAndConditionsAccepted(true)
-                .planData(new PlanDataModel()
-                        .totalAmount(new BigDecimal(10))
-                        .numberOfInstallments(10)
-                        .currency("USD")
-                        .purchaseMethod(PurchaseMethod.INSTORE)
-                )
-                .shopper(new ShopperData()
-                        .email("fake@email.com")
-                )
-                .billingAddress(new AddressDataModel()
-                        .addressLine1("144 Union St")
-                        .city("Brooklyn")
-                        .state("North Dakota")
-                        .zip("11231")
-                        .country("United States")
-                )
-                .paymentMethod(new PaymentMethodModel()
-                        .type(PaymentMethodType.CARD)
-                        .card(new CardData()
-                                .cardExpMonth("12")
-                                .cardExpYear("2025")
-                                .cardCvv("111")
-                                .cardNumber("4556997457604103")
-                                .cardHolderFullName("Test User")
-                        )
-                );
-        InstallmentPlanModel result = api.post(new Date().toString(), installmentPlanCreateRequest, null);
-        System.out.println(result);
-        assertNotNull("Received null response", result);
-    }
+        /**
+         * @throws ApiException if the Api call fails
+         */
+        @Test
+        public void createInstallmentPlanTest() throws ApiException {
+                String clientId = System.getenv("SPLITIT_CLIENT_ID");
+                String secretId = System.getenv("SPLITIT_CLIENT_SECRET");
+                ApiClient defaultClient = new ApiClient(clientId, secretId, null);
+                InstallmentPlanApi api = new InstallmentPlanApi(defaultClient);
+                InstallmentPlanInitiateRequest installmentPlanCreateRequest = new InstallmentPlanInitiateRequest();
+                installmentPlanCreateRequest.attempt3dSecure(true)
+                                .autoCapture(true)
+                                .attempt3dSecure(true)
+                                .planData(new PlanDataModel()
+                                                .totalAmount(new BigDecimal(10))
+                                                .numberOfInstallments(10)
+                                                .currency("USD")
+                                                .purchaseMethod(PurchaseMethod.INSTORE))
+                                .shopper(new ShopperData()
+                                                .email("fake@email.com"))
+                                .billingAddress(new AddressDataModel()
+                                                .addressLine1("144 Union St")
+                                                .city("Brooklyn")
+                                                .state("North Dakota")
+                                                .zip("11231")
+                                                .country("United States"))
+                                .redirectUrls(new InitiateRedirectionEndpointsModel());
+                InitiatePlanResponse result = api.post(new Date().toString(), installmentPlanCreateRequest).execute();
+                System.out.println(result);
+                assertNotNull("Received null response", result);
+        }
 }
