6980fd65e3f5c9bed01b5706457f669836f252bb
eddiechayes <eddiechayes@gmail.com>
2023-10-24 16:28:17 -0700

    fix handwritten test

--- java/src/test/java/com/konfigthis/splitit/client/api/SimpleInstallmentPlanApiTest.java
+++ java/src/test/java/com/konfigthis/splitit/client/api/SimpleInstallmentPlanApiTest.java
@@ -2,6 +2,7 @@ package com.konfigthis.splitit.client.api;
 
 import com.konfigthis.splitit.client.ApiClient;
 import com.konfigthis.splitit.client.ApiException;
+import com.konfigthis.splitit.client.auth.OAuth;
 import com.konfigthis.splitit.client.model.*;
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +17,15 @@ public class SimpleInstallmentPlanApiTest {
          */
         @Test
         public void createInstallmentPlanTest() throws ApiException {
-                String clientId = System.getenv("SPLITIT_CLIENT_ID");
-                String secretId = System.getenv("SPLITIT_CLIENT_SECRET");
-                ApiClient defaultClient = new ApiClient(clientId, secretId, null);
+                // String clientId = System.getenv("SPLITIT_CLIENT_ID");
+                // String secretId = System.getenv("SPLITIT_CLIENT_SECRET");
+                ApiClient defaultClient = new ApiClient();
+                defaultClient.setBasePath("http://127.0.0.1:4010");
+                OAuth auth = (OAuth) defaultClient.getAuthentication("oauth");
+                auth.setAccessToken("dummy_token");
                 InstallmentPlanApi api = new InstallmentPlanApi(defaultClient);
                 InitiatePlanResponse result = api
-                                .post(true, new Date().toString())
+                                .post(true, new Date().toString(), new Date().toString())
                                 .attempt3dSecure(true)
                                 .planData(new PlanDataModel()
                                                 .totalAmount(10)
