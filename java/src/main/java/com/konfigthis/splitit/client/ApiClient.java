d51275aa05c788944d30d49a45fe601a32820036
eddiechayes <eddiechayes@gmail.com>
2023-11-01 16:54:18 -0700

    Version Bump Java SDK

--- java/src/main/java/com/konfigthis/splitit/client/ApiClient.java
+++ java/src/main/java/com/konfigthis/splitit/client/ApiClient.java
@@ -213,7 +213,7 @@ public class ApiClient extends ApiClientCustom {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Konfig/2.2.1/java");
+        setUserAgent("Konfig/2.2.2/java");
 
         authentications = new HashMap<String, Authentication>();
     }
