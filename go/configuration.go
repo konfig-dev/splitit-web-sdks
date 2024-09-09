bedbc53f119cf4a99d2ce2ac316bad9ab6cec6be
konfig-publisher <publisher@konfigthis.com>
2024-09-09 20:45:10 +0000

    Version Bump SDKs

--- go/configuration.go
+++ go/configuration.go
@@ -104,7 +104,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "Konfig/1.8.2/go",
+		UserAgent:        "Konfig/1.8.3/go",
 		TokenUrl:   	  "https://id.production.splitit.com/connect/token",
 		Debug:            false,
         Context: context.Background(),
--- typescript/README.md
+++ typescript/README.md
@@ -2,7 +2,7 @@
 
 Splitit's Web API
 
-[![npm](https://img.shields.io/badge/npm-v4.0.3-blue)](https://www.npmjs.com/package/splitit-web-typescript-sdk/v/4.0.3)
+[![npm](https://img.shields.io/badge/npm-v4.0.4-blue)](https://www.npmjs.com/package/splitit-web-typescript-sdk/v/4.0.4)
 
 ## Table of Contents<a id="table-of-contents"></a>
 
